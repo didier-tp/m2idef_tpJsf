4c6c0c9e18bd15fac0562ac48c81429c13037659
formateur fou <me@ici_ou_labas.fr>
2018-04-24 13:34:20 +0200

    nouvelle version

--- init_push.bat
+++ init_push.bat
@@ -2,7 +2,7 @@ cd /d "%~dp0"
 git init
 git add *
 git commit -a -m "version initiale"
-git remote add gitHubOriginTpJsf https://didier-tp:pwd007!@github.com/didier-tp/m2idef_tpJsf.git
+git remote add gitHubOriginTpJsf https://didier-tp:password@github.com/didier-tp/m2idef_tpJsf.git
 git push -u gitHubOriginTpJsf master
 pause
 
