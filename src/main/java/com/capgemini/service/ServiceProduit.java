059adf7ca9da0ab9cfc32b4a96679fe9b5c110d3
formateur fou <me@ici_ou_labas.fr>
2018-04-25 13:59:35 +0200

    nouvelle version

--- src/main/java/com/capgemini/service/ServiceProduit.java
+++ src/main/java/com/capgemini/service/ServiceProduit.java
@@ -6,19 +6,39 @@ import java.util.List;
 import com.capgemini.entity.Produit;
 
 /*
- Business Service (plus tard pris en charge par Spring)
+ Business Service (plus tard pris en charge par Spring ou EJB)
  gérant des produits classés par categorie
  Dans la version temporaire sans spring : design pattern Singleton 
   + jeu de données en dur (simulation sans base de données)
  */
 public class ServiceProduit {
 	
-	private List<Produit> produits; //+get/set 
+	private static ServiceProduit uniqueInstance=null;
+	
+	public static synchronized ServiceProduit getInstance() {
+		if(uniqueInstance==null) {
+			uniqueInstance=new ServiceProduit();
+		}
+		return uniqueInstance;
+	}
+	
+	private List<Produit> produits; //+get/set ou ...
+	
+	public List<Produit> rechercherProduitsParCategorie(Long catId){
+		//version 1 : sans tenir compte de catId
+		return this.produits;
+	}
 	
 	public ServiceProduit(){
 		produits=new ArrayList<Produit>();
 		produits.add(new Produit(1L,"cahier" , 2.5 ,"grands carreaux"));
-		//...add , add , add
+		produits.add(new Produit(2L,"stylo" , 1.5 ,"bleu"));
+		
+		produits.add(new Produit(11L,"film 1" , 12.5 ,"DVD"));
+		produits.add(new Produit(12L,"film 2" , 11.5 ,"blue ray"));
+		
+		produits.add(new Produit(21L,"livre 1" , 7.5 ,"Roman"));
+		produits.add(new Produit(22L,"livre 2" , 6.5 ,"BD"));
 	}
 
 }
