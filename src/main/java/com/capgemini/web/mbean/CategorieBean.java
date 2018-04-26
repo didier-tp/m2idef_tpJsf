8913970e275c6781144a4857c1a08b9cfb431f1d
formateur fou <me@ici_ou_labas.fr>
2018-04-26 16:35:44 +0200

    nouvelle version

--- src/main/java/com/capgemini/web/mbean/CategorieBean.java
+++ src/main/java/com/capgemini/web/mbean/CategorieBean.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.event.ValueChangeEvent;
 
 import com.capgemini.entity.Categorie;
@@ -16,18 +18,21 @@ public class CategorieBean {
 	private List<Categorie> listeCategorie; //pour remplir valeurs selectionnables
     //dans liste  (+get/set)
 
-    private Categorie categorie;
-    private Long idCategorie;
+    private Categorie categorie=null;
+    private Long idCategorie=null;
+    private Boolean bNew = Boolean.FALSE;
     
     private ServiceProduit serviceProduit;
     
     public CategorieBean() {
     	this.serviceProduit = ServiceProduit.getInstance();//singleton
     	this.listeCategorie = this.serviceProduit.rechercherListeCategories();
+    	/*
     	if(this.listeCategorie!=null && !this.listeCategorie.isEmpty()) {
     	    this.categorie = this.listeCategorie.get(0);
     	    this.idCategorie = this.categorie.getIdCat();
-    	}
+    	}*/
+    	this.categorie = new Categorie();
     }
     
     private void selectCategorieFromIdCategorie() {
@@ -39,11 +44,44 @@ public class CategorieBean {
     	}
     }
     
+    public void onNewCategorie(ActionEvent event) {
+		System.out.println("onNewCategorie() ");
+		this.categorie = new Categorie();
+		this.bNew = true;
+	}
+    
+    public void onAddCategorie(ActionEvent event) {
+		System.out.println("onAddCategorie() , categorie=" + this.categorie);
+		if(this.categorie.getLabel()!=null && !this.categorie.getLabel().isEmpty()
+				&& this.categorie.getIdCat() != null && this.categorie.getIdCat()>0) {
+				this.listeCategorie.add(categorie);
+				this.idCategorie = this.categorie.getIdCat();
+				this.bNew = false;
+		}
+	}
+    
+    
+    public void onUpdateCategorie(ActionEvent event) {
+		System.out.println("onUpdateCategorie() ");
+		//...
+	}
+    
+    public void onDeleteCategorie(ActionEvent event) {
+		System.out.println("onDeleteCategorie() ");
+		if(this.listeCategorie.contains(this.categorie)) {
+			this.listeCategorie.remove(this.categorie);
+			this.categorie = new Categorie();
+			this.idCategorie = null;
+		}
+	}
+    
     public void onCategorieChange(ValueChangeEvent event) {
 		this.idCategorie = (Long) event.getNewValue();
 		System.out.println("onCategorieChange() idCategorie=" + idCategorie);
 		selectCategorieFromIdCategorie();
 	}
+    
+    
 
 	public List<Categorie> getListeCategorie() {
 		return listeCategorie;
@@ -67,6 +105,14 @@ public class CategorieBean {
 
 	public void setIdCategorie(Long idCategorie) {
 		this.idCategorie = idCategorie;
+	}
+
+	public Boolean getbNew() {
+		return bNew;
+	}
+
+	public void setbNew(Boolean bNew) {
+		this.bNew = bNew;
 	}
 
     
